6217b3c7af477fcdc17ad719237cceccb17e032c
lathifa26 <92505141+lathifa26@users.noreply.github.com>
2022-12-09 20:45:21 +0530

    img tag changed

--- src/Result.js
+++ src/Result.js
@@ -4,7 +4,7 @@ function Result({ result, openDetail }) {
   return (
     <div className="result" onClick=
         {() => openDetail(result.imdbID)}>
-      <img src={result.Poster} />
+      <img src={result.Poster}  alt="movie poster"/>
       <h3>{result.Title}</h3>
     </div>
   );
